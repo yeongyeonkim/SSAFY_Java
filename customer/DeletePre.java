5aea1b9e9cceeeef740affb21ae2e6ede1d8a8d4
student <student@M804>
2019-08-21 15:52:55 +0900

    PreparedStatement는 Statement의 상속을 받아 더 많은, 빠른 기능을 할 수 있다. Run

--- customer/DeletePre.java
+++ customer/DeletePre.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 
-public class InsertPre2 {
+public class DeletePre {
 
 	public static void main(String[] args) {
 		String driver = "com.mysql.cj.jdbc.Driver";
@@ -12,8 +12,7 @@ public class InsertPre2 {
 		String user = "scott";
 		String password = "tiger";
 		
-		//insert into customer values(2, 'jane', 'la')
-		String query = "insert into customer values(?,?,?)";
+		String query = "delete from customer where num = ?";
 		
 		try {
 			//1. Driver 등록 : 사용할 db driver 등록
@@ -26,15 +25,12 @@ public class InsertPre2 {
 //			Statement stat = con.createStatement();
 			PreparedStatement pstat = con.prepareStatement(query);
 			
-			//3-1. ?에 값 setting
-			pstat.setString(1, args[0]); 
-			pstat.setString(2, args[1]); // string이니까 '와 ,같은 것을 자동으로 붙여줌 'tommy'
-			pstat.setString(3, args[2]);
+			//3-1. ?로?
+			pstat.setString(1, args[0]);
 			
 			//4. Query 실행
-//			int rs = stat.executeUpdate(query);//insert
-			int rs = pstat.executeUpdate();
-			System.out.println(rs + "개의 레코드 추가!");
+			int rs = pstat.executeUpdate();//insert
+			System.out.println(rs + "개의 레코드 삭제!");
 			
 			//5. 마무리
 			pstat.close();
